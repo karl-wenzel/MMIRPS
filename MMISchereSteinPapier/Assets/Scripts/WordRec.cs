6f9dc1e533c9a934b3156eed7f85dacd4a7826b8
lynn-2019 <l-habermann@hotmail.de>
2025-06-09 12:08:00 +0200

    Task2: Use KeywordRecognizer

---
+++ MMISchereSteinPapier/Assets/Scripts/WordRec.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+using System.Linq;
+
+public class WordRec : MonoBehaviour
+{
+    // Define keywords (added cutter here as an extra for scissors)
+    private readonly string[] keywords = { "rock", "paper", "scissors", "cutter", "again", "go" };
+
+    // Using KeywordRecognizer
+    private KeywordRecognizer keywordRecognizer;
+
+    void Start()
+    {
+        // Initialize the KeywordRecognizer with the list of keywords.
+        keywordRecognizer = new KeywordRecognizer(keywords);
+        keywordRecognizer.OnPhraseRecognized += OnPhraseRecognized;
+        keywordRecognizer.Start();
+
+        // Introtext Terminal
+        Debug.Log("Speech recognizer started. Please say 'rock', 'paper', or 'scissors'.");
+    }
+
+    void OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        Debug.Log("Recognized: " + args.text);
+
+        //Case 1: Go again button ToDo: Only make it an option when game is not running
+        if (args.text.ToLower() == "go")
+        {
+            GameManager.instance.ExecuteGameLoop();
+        }
+        // ToDo: Only make it an option when game is running
+        else
+        {
+            // Case 2: in game
+            // Convert the recognized text to an RPSOption.
+            RPSOption option = ConvertKeywordToOption(args.text);
+
+            // Choose  a button
+            GameManager.instance.OnOptionChosen(option);
+        }
+
+    }
+
+    RPSOption ConvertKeywordToOption(string keyword)
+    {
+        switch (keyword.ToLower())
+        {
+            case "rock":
+                return RPSOption.Rock;
+            case "paper":
+                return RPSOption.Paper;
+            case "scissors":
+                return RPSOption.Scissors;
+            case "cutter":
+                return RPSOption.Scissors;
+            default:
+                Debug.LogWarning("Unrecognized keyword: " + keyword);
+                return default;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (keywordRecognizer != null && keywordRecognizer.IsRunning)
+        {
+            Debug.Log("Speech recognizer ended");
+            keywordRecognizer.Stop();
+            keywordRecognizer.OnPhraseRecognized -= OnPhraseRecognized;
+        }
+    }
+}
